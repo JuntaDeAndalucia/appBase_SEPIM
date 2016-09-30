ab36299bc811819bfcc241fcdd0f03f1dbe69168
unknown <farrebola@ieca-1346.intranet.ieca.junta-andalucia.es>
2016-09-30 09:30:06 +0200

    Configura m4

--- js/config.js
+++ js/config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //******** configuración **********************//
 url = "http://mapea-sigc.juntadeandalucia.es/sepim_server/api";
 urlGB = "http://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/[CORE]"; //JGL - vacío si no tiene GB
@@ -18,25 +17,4 @@ showDistance = false; //JGL - indica si mostrar la distancia o no en la búsqued
 gbRows = 150;
 M.proxy(false);
 attrNotShow = [ "the_geom", "geom", "_version_", "solrid", "keywords", "equipamiento"];
-=======
-//******** configuración **********************//
-url = "http://mapea-sigc.juntadeandalucia.es/sepim_server/api";
-urlGB = "http://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/[CORE]"; //JGL - vacío si no tiene GB
-
-idAplicacion = [ID]; //id de la aplicación
-//*********************************************//
-aplicacion = null;
-pilaCategorias = [];
-coor_x = null;
-coor_y = null;
-idEntidad = null;
-datos = {
-	limit: -1,
-	offset:0
-};
-showDistance = false; //JGL - indica si mostrar la distancia o no en la búsqueda por categorías
-gbRows = 150;
-M.proxy(false);
-attrNotShow = [ "the_geom", "geom", "_version_", "solrid", "keywords", "equipamiento"];
->>>>>>> upstream/master
 window.isApp 	= /^(?!HTTP)/.test(document.URL.toUpperCase()); 
